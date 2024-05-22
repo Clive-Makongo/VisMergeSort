b807a1958854de800827c888dcec238dc2811497
Clive-Makongo <clivemakongo@gmail.com>
2024-05-22 22:17:44 +0100

    trying first round of sorting

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Display from './assets/Pages/Display'
 import Home from './assets/Pages/Home'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
