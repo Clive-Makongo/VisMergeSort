86cfa32e567cc73e4f79fecfe995b2391962698b
Clive-Makongo <clivemakongo@gmail.com>
2024-05-24 13:25:17 +0100

    trying to map pairs

--- src/assets/Pages/NextSort.jsx
+++ src/assets/Pages/NextSort.jsx
@@ -8,17 +8,55 @@ import { AnimatePresence, motion } from 'framer-motion';
 import { Modal, Button } from 'react-bootstrap';
 
 const sort = (props) => {
+    const [isClicked, setClicked] = useState([]);
     const { swapped, clicked, grandChildren } = props;
     console.log(grandChildren, ` :sort`);
+
+    useEffect(() => {
+        setClicked(clicked);
+        console.log(clicked, ` :useEffect`);
+    }, [props]);
 };
 
 const NextSortPage = (props) => {
     const [grandChildren, setGrandChildren] = useState([]);
+    const [paired, setPaired] = useState([]);
+    const [pairedLoaded, setPairedLoaded] = useState(false);
+
     console.log(props, ` :NextSortPage`);
 
     useEffect(() => {
         setGrandChildren(props.grandChildren);
+
+        let arr = [];
+        for (let i = 0; i < grandChildren.length; i++) {
+            arr[i] = [grandChildren[i].firstChild, grandChildren[i].secondChild];
+        };
+
+        let arr2 = [];
+        let temp = [];
+        arr.forEach((el, index) => {
+            temp.push(el);
+            if ((index + 1) % 2 === 0) {
+                arr2.push(temp);
+                temp = [];
+            }
+        });
+
+        // Push the remaining elements if any
+        if (temp.length > 0) {
+            arr2.push(temp);
+        }
+        console.log(`Arr2: `, arr2);
+
+        setPaired([...arr2]);
+        setPairedLoaded(true);
     }, [props]);
+
+    useEffect(() => {
+        console.log(`Paired: `, paired);
+        console.log(`Paired Loaded: `, pairedLoaded);
+    }, [paired, pairedLoaded]);
 
 
 
@@ -29,18 +67,15 @@ const NextSortPage = (props) => {
             <>
                 <div className='row d-flex flex-row'>
                     <div className='col-md-12 d-flex flex-row'>
-                        {grandChildren.map((el, index) => {
-                            return (
-                                <>
-                                    <div className='col-md-1'>
-                                        <p>{el.firstChild}</p>
-                                    </div>
-                                    <div className='col-md-1'>
-                                        <p>{el.secondChild}</p>
-                                    </div>
-                                </>
-                            );
-                        })}
+                        {pairedLoaded &&
+                            <>
+                                {paired.map((el, index) => {
+                                    <>
+                                        
+                                    </>
+                                })}
+                            </>
+                        }
                     </div>
                 </div>
             </>
